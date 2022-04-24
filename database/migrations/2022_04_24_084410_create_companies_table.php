811ffae06ffa02382d43bfe8a11acffbf065606b
mosaababneh <52359791+mosaababneh@users.noreply.github.com>
2022-04-24 16:29:45 -0700

    Update 2022_04_24_084410_create_companies_table.php

--- database/migrations/2022_04_24_084410_create_companies_table.php
+++ database/migrations/2022_04_24_084410_create_companies_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('companies', function (Blueprint $table) {
+       Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('Name');
-            $table->string('email');
-            $table->string('logo');
-            $table->string('website');
+            $table->string('email')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('website')->nullable();
 
             $table->timestamps();
         });
